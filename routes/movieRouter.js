df68cc533e05a6db1438df13d7f7fa9f70ef09e7
chank52 <chank52@student.douglascollege.ca>
2023-08-07 00:37:35 -0700

    Update movieRouter.js
    Updated
    - Movie model, router

--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -4,50 +4,37 @@ const axios = require("axios");
 require('dotenv').config();
 
 // ---------------------------------------------------------
-// Get movies from tmdb
+// Get from tmdb and save to local db  (currently get 200 movies from tmdb)
 // ---------------------------------------------------------
-router.route("/getfromtmdb").get((req, res) => {
-   
-    console.log("*** Get from TMDB: ");
-
-    const tmdbMovieListURI = process.env.TMDBMovieListURI;
-
-    const objList = axios  (tmdbMovieListURI);
- 
-    console.log(objList);
-    
-    // // Check object
-    // objModel.find({ }, (err, users) => {
-    //   if (err) {
-    //       console.error('Error finding info:', err);
-    //       return res.status(400).json({ error: 'Error' });
-    //   } 
-    //   else {
-    //       if(users.length > 0){
-    //         return res.status(200).json(users);
-    //     }
-    //       else{
-    //           return res.status(400).json({ error: 'Data not found' });
-    //       }
-    //   }
-    // });
-  
-});
-
 router.get('/get-from-tmdb-to-local', async (req, res) => {
     try {
       const tmdbMovieListURI = process.env.TMDBMovieListURI;
 
       // Fetch data from the API using axios
-      const response = await axios.get(tmdbMovieListURI);
-      const apiData = response.data.results;
+      let response = await axios.get(tmdbMovieListURI);
+      let apiData = response.data.results;
   
       await objModel.deleteMany({});
 
       // Save the fetched data to MongoDB using Mongoose
       const savedData = await objModel.create(apiData);
-  
-      res.json(savedData);
+
+      const fetchLoopCount = 9;
+      
+      for(i=0; i< fetchLoopCount; i++){
+
+        // Fetch data from the API using axios
+            uri = tmdbMovieListURI.substring(0, tmdbMovieListURI.length -1) + (i+2);
+            //console.log(uri + (i+2));
+            response = await axios.get(uri);
+            apiData = response.data.results;
+            await objModel.insertMany(apiData);
+        
+      }
+      
+      // return
+      return res.status(200).json({ data: 'OK' });
+      
     } catch (error) {
       console.error('Error fetching data from the API and saving to MongoDB:', error);
       res.status(500).json({ error: 'Failed to fetch and save data' });
@@ -62,14 +49,14 @@ router.route("/getall").get((req, res) => {
     console.log("*** List all: ");
   
     // Check object
-    objModel.find({ }, (err, users) => {
+    objModel.find({ }, (err, objs) => {
       if (err) {
           console.error('Error finding info:', err);
           return res.status(400).json({ error: 'Error' });
       } 
       else {
-          if(users.length > 0){
-            return res.status(200).json(users);
+          if(objs.length > 0){
+            return res.status(200).json(objs);
         }
           else{
               return res.status(400).json({ error: 'Data not found' });
@@ -106,66 +93,5 @@ router.route("/add").post((req, res) => {
 
 });
 
-// ---------------------------------------------------------
-// Login : Get object
-// ---------------------------------------------------------
-router.route("/login").get((req, res) => {
-    const userId = req.body.userId;
-    const password = req.body.password;
-   
-    console.log("*** Login: " + userId);
-  
-    // Check object
-    objModel.find({ userId: userId }, (err, users) => {
-      if (err) {
-          console.error('Error finding info:', err);
-          return res.status(400).json({ error: 'Error' });
-      } 
-      else {
-          if(users.length > 0){
-            const authenticated = bcryptjs.compareSync(password, users[0].password);
-            if (authenticated){
-                return res.status(200).json(users);
-            }
-            else{
-                return res.status(400).json({error: 'password error'});
-            }
-          }
-          else{
-              return res.status(400).json({ error: 'User not found' });
-          }
-      }
-    });
-  
-});
-
-// ---------------------------------------------------------
-// Update object
-// ---------------------------------------------------------
-router.route("/update").post((req, res) => {
-    const userId = req.body.userId;
-    const password = req.body.password;
-   
-    console.log("*** Update: " + userId);
-  
-    // Check object
-    objModel.findOne({ userId: userId }, (err, obj) => {
-      if (err) {
-          console.error('Error finding info:', err);
-          return res.status(400).json({ error: 'Error' });
-      } 
-      else {
-          if(obj != null && obj.userId.length > 0){
-
-                obj.password = bcryptjs.hashSync(password,10);
-                obj.save();
-                return res.status(200).json(obj);
-          }
-          else{
-              return res.status(400).json({ error: 'User not found' });
-          }
-      }
-    });
-});
 
 module.exports = router;
